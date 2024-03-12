5c28c32d916ce624a77391d09ecb28355d321da0
LawsonGod <godwill.lawson@gmail.com>
2024-03-12 16:18:18 +0100

    1er version header

--- includes/headerNav.php
+++ includes/headerNav.php
@@ -1,1 +1,77 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-compatible" content="ie-edge">
+    <title>Header</title>
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+    <body>
+        <div class='container p-0 mt-2 rounded'>
+            <header class='bg-light text-black rounded-top'>
+                <div class='row align-items-center justify-content-between m-0'>
+                    <div class='col-auto p-2 text-center'>
+                        <a href='index.php'>
+                            <img src='' class='img-fluid' alt='logo du site' />
+                        </a>
+                    </div>
+                    <div class='col d-flex justify-content-center'>
+                        <form class="form-inline d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div class='col-auto text-right pt-1 pr-4 connexion-section'>
+                            <a href="" class="d-block">
+                                <img src="" class="connexion-logo" alt="connexion-logo" alt="connexion">
+                            </a>
+                            <li class="d-block nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Vous êtes une école
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="">Formation</a></li>
+                                    <li><a class="dropdown-item" href="">Intervention</a></li>
+                                </ul>
+                            </li>
+                    </div>   
+                </div>
+            </header>
+        </div>
+        <div class="container p-0 rounded">
+            <nav class="navbar navbar-expand-md navbar-light bg-light rounded-bottom">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-black" href=" ">Actualités</a>   
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-black" href=" ">Forum</a>   
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black" href=" ">Rendez-vous</a>   
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black" href=" ">Qui sommes-nous ?</a>   
+                        </li>
+                       
+                    </ul>
+                </div>
+
+            </nav>
+        </div>
+        
+       
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    </body>
+</html>
