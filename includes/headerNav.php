a7a6371b17579794d772f3677fb770749222cb71
LawsonGod <godwill.lawson@gmail.com>
2024-03-12 22:57:26 +0100

    Header version 2

--- includes/headerNav.php
+++ includes/headerNav.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-compatible" content="ie-edge">
-    <title>Header</title>
+    <title>Balance Ton Bully - Header</title>
     <link href="../css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet">
 </head>
     <body>
         <div class='container p-0 mt-2 rounded'>
@@ -14,20 +16,26 @@
                 <div class='row align-items-center justify-content-between m-0'>
                     <div class='col-auto p-2 text-center'>
                         <a href='index.php'>
-                            <img src='' class='img-fluid' alt='logo du site' />
+                            <img src='../assets/Logo_site.png' class='img-fluid perso_logoSize' alt='logo du site' />
                         </a>
                     </div>
                     <div class='col d-flex justify-content-center'>
                         <form class="form-inline d-flex">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                            <a href="#!" class="d-flex align-items-center">
+                                <i class="bi bi-search" style="font-size: 1.5rem;"></i>
+                            </a>
                         </form>
                     </div>
-                    <div class='col-auto text-right pt-1 pr-4 connexion-section'>
-                            <a href="" class="d-block">
-                                <img src="" class="connexion-logo" alt="connexion-logo" alt="connexion">
+
+                    <div class='col-auto text-right pt-1 pr-4 connexion-section '>
+                        <div class="d-flex justify-content-center custom-bg-logo rounded-top" >
+                            <a href="" class="d-block" style="max-width: 50px;">
+                                <img src="../assets/🦆 icon _people_.png" class="connexion-logo img-fluid" alt="connexion-logo" alt="connexion">
                             </a>
-                            <li class="d-block nav-item dropdown">
+                        </div>
+                        <div>
+                            <div class="dropdown custom-bg-dropdown rounded-bottom">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Vous êtes une école
                                 </a>
@@ -35,7 +43,8 @@
                                     <li><a class="dropdown-item" href="">Formation</a></li>
                                     <li><a class="dropdown-item" href="">Intervention</a></li>
                                 </ul>
-                            </li>
+                            </div>
+                        </div>  
                     </div>   
                 </div>
             </header>
@@ -49,17 +58,16 @@
                 <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link text-black" href=" ">Actualités</a>   
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link text-black" href=" ">Forum</a>   
+                            <a class="nav-link text-black text-center mx-5" href="">Actualités</a>   
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-black" href=" ">Rendez-vous</a>   
+                            <a class="nav-link text-black text-center mx-5" href="">Forum</a>   
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-black" href=" ">Qui sommes-nous ?</a>   
+                            <a class="nav-link text-black text-center mx-5" href="">Rendez-vous</a>   
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black text-center mx-5" href="">Qui sommes-nous ?</a>   
                         </li>
                        
                     </ul>
@@ -71,7 +79,5 @@
        
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     </body>
 </html>
