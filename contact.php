987077bd6c82bfb6a664853e90e2a423c473da44
WillyB89 <walid.ben-aissa@edu.esiee-it.fr>
2024-03-14 10:22:35 +0100

    Maj v1(.html=>.php) page contact

--- contact.php
+++ contact.php
@@ -4,9 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contact</title>
-  <link rel="stylesheet" href="style-contact.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+  <link rel="stylesheet" href="../css/style.css">
   <h1>Contactez-nous</h1>
   <p>N'hésitez pas à nous contacter en utilisant le formulaire ci-dessous.</p>
 
