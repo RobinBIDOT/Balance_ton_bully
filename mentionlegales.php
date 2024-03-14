6590744e15ed16d6fa0744f22cd4be37de16521c
WillyB89 <walid.ben-aissa@edu.esiee-it.fr>
2024-03-14 10:20:23 +0100

    Maj v1 (.html=>.php)

--- mentionlegales.php
+++ mentionlegales.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mentions Légales</title>
-    <link rel="stylesheet" href="style-mentionLegales.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+    <?php include('../includes/headerNav.php')?>
     <h1>Mentions Légales</h1>
 
     <h2>Définitions</h2>
