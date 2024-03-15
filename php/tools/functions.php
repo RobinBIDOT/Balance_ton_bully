28f432bb61c0aea6d1c88fb967ecd145f66f3520
WillyB89 <walid.ben-aissa@edu.esiee-it.fr>
2024-03-15 15:14:59 +0100

    maj style.css

--- php/tools/functions.php
+++ php/tools/functions.php
@@ -1,6 +1,6 @@
 <?php
 function dbConnexion(){
-    $file_path = 'c:/xampp/htdocs/Balance_ton_bully/php/tools/info.json';
+    $file_path = 'Applications/xampp/htdocs/Balance_ton_bully/php/tools/info.json';
     $tool = json_decode(file_get_contents($file_path), true);
     $dsn = "mysql:host=" . $tool['host'] . ";dbname=". $tool['database'] . ";port=". $tool['port'] . ";charset=". $tool['charset'];
     $user = $tool['user'];
