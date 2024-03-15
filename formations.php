e215dd4649b08afdb524ab1277ba63bcea0ce2bf
WillyB89 <walid.ben-aissa@edu.esiee-it.fr>
2024-03-15 15:14:59 +0100

    maj style.css

--- formations.php
+++ formations.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Demande de formation</title>
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="style-formations.css">
 </head>
 <body>
   <?php include('../includes/headerNav.php')?>
