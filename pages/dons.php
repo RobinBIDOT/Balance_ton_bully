68a563de929cb864856ea61843adc09e860b2721
Robin BIDOT <rbidot@hotmail.fr>
2024-03-18 20:43:31 +0100

    refactor : ajouter le lien vers la page dons dans le footer, travailler sur le front de la page dons

--- pages/dons.php
+++ pages/dons.php
@@ -1,14 +1,3 @@
-<?php
-    // Inclusion du fichier de connexion à la base de données
-    include('../php/tools/functions.php');
-    $dbh = dbConnexion();
-    session_start();
-
-    // Afficher les informations de session
-    //    echo "<pre>";
-    //    var_dump($_SESSION);
-    //    echo "</pre>";
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -23,6 +12,10 @@
             padding: 20px;
             color: white;
             border-radius: 10px;
+        }
+        .btn-selected {
+            background-color: #17a2b8 !important;
+            border-color: #17a2b8 !important;
         }
     </style>
 </head>
@@ -45,17 +38,21 @@
                         <input type="radio" name="montant" value="20"> 20 €
                         <input type="radio" name="montant" value="50"> 50 €
                         <input type="radio" name="montant" value="150"> 150 €<br>
-                        <input type="text" class="form-control mt-2" id="montantLibre" placeholder="Montant libre">
+                        <label for="montantLibre" class="form-label">Montant libre</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="montantLibre" placeholder="Montant libre">
+                            <button type="button" id="confirmMontantLibre" class="btn btn-primary">Confirmer</button>
+                        </div>
                     </div>
                 </form>
             </div>
             <div class="blue-background mt-5">
                 <form>
                     <h3>Réduction fiscale</h3>
-                    <p>Le don à Balance ton Bully ouvre le droit à une réduction fiscale car il remplit les conditions générales prévues aux articles 200 et 238 bis du code général des impôts.</p>
+                    <p id="infoReduction"></p>
                     <button type="button" id="btnParticulier" class="btn btn-primary">Particulier</button>
                     <button type="button" id="btnOrganisme" class="btn btn-primary">Organisme</button>
-                    <p id="infoReduction"></p>
+                    <p id="infoReductionCategorie"></p>
                 </form>
             </div>
         </div>
@@ -65,6 +62,11 @@
             <div class="blue-background" id="mesCoordonnees">
                 <form>
                     <h3>Mes coordonnées</h3>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="payerOrganisme">
+                        <label class="form-check-label" for="payerOrganisme">Payer en tant qu'organisme</label>
+                    </div>
+                    <!-- Ajout du formulaire pour les organismes -->
                     <div id="champsOrganisme" style="display: none;">
                         <div class="mb-3">
                             <label for="raisonSociale" class="form-label">Raison sociale</label>
@@ -79,6 +81,7 @@
                             <input type="text" class="form-control mb-2" placeholder="Forme juridique">
                         </div>
                     </div>
+                    <!-- Fin du formulaire pour les organismes -->
                     <div class="mb-3">
                         <label for="prenom" class="form-label">Prénom</label>
                         <input type="text" class="form-control" id="prenom" name="prenom" required>
@@ -142,8 +145,6 @@
 </div>
 <?php include('../includes/footer.php') ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function() {
         // Récupération des éléments du DOM
@@ -154,88 +155,154 @@
         let infoReduction = $('#infoReduction');
         let montants = $('input[name="montant"]');
         let montantLibre = $('#montantLibre');
-        let divCoordonnees = $('#mesCoordonnees');
-        let recapDon = $('#recapDon');
         let typeDon = $('#typeDon');
         let montantTotal = $('#montantTotal');
         let validerPayer = $('#validerPayer');
+        let payerOrganismeCheckbox = $('#payerOrganisme');
+        let champsOrganisme = $('#champsOrganisme');
+        let infoReductionCategorie = $('#infoReductionCategorie');
 
         // Fonction pour ajouter des champs pour les organismes
-        function ajouterChampsOrganisme() {
-            if ($('#champsOrganisme').children().length === 0) {
-                divCoordonnees.append(`
-                <div id="champsOrganisme">
-                    <div class="mb-3">
-                        <label for="raisonSociale" class="form-label">Raison sociale</label>
-                        <input type="text" class="form-control mb-2" placeholder="Raison sociale">
-                    </div>
-                    <div class="mb-3">
-                        <label for="siren" class="form-label">SIREN</label>
-                        <input type="text" class="form-control mb-2" placeholder="SIREN">
-                    </div>
-                    <div class="mb-3">
-                        <label for="formeJuridique" class="form-label">Forme juridique</label>
-                        <input type="text" class="form-control mb-2" placeholder="Forme juridique">
-                    </div>
-                </div>
-            `);
+        function afficherChampsOrganisme() {
+            champsOrganisme.show();
+        }
+
+        function cacherChampsOrganisme() {
+            champsOrganisme.hide();
+        }
+
+        // Mise à jour de l'affichage en fonction du type de paiement
+        function miseAJourAffichagePaiement() {
+            if (payerOrganismeCheckbox.is(':checked')) {
+                afficherChampsOrganisme();
+            } else {
+                cacherChampsOrganisme();
             }
         }
 
-        // Fonction pour changer les montants des dons
-        function changerMontants(montantsDon) {
-            montants.each(function() {
-                let montantInput = $(this);
-                let valeur = montantsDon[montantInput.val()];
-                if (valeur) {
-                    montantInput.val(valeur).next().text(' ' + valeur + ' €');
+        // Fonction pour calculer la réduction fiscale en fonction du type de paiement
+        function calculerReductionFiscale(montant) {
+            // Vérifier si un montant valide est saisi
+            if (!isNaN(montant)) {
+                let reduction;
+                if (payerOrganismeCheckbox.is(':checked')) {
+                    // Si le paiement est fait en tant qu'organisme
+                    reduction = montant * 0.60; // 60% de réduction pour les organismes
+                } else {
+                    // Si le paiement est fait en tant que particulier
+                    reduction = montant * 0.66; // 66% de réduction pour les particuliers
                 }
-            });
+                return (montant - reduction).toFixed(2);
+            } else {
+                return "0.00"; // Retourner 0 si aucun montant n'est saisi
+            }
         }
 
-        // Mise à jour de l'information fiscale et du montant après réduction
-        function miseAJourInfoFiscale(montant) {
-            let montantReduit = btnParticulier.is(':checked') ? montant * 0.66 : montant * 0.60;
-            infoReduction.text(`Votre don ne vous coûtera que ${montantReduit.toFixed(2)} € après réduction fiscale`);
+        // Événement pour la case à cocher "Payer en tant qu'organisme"
+        payerOrganismeCheckbox.change(function() {
+            let montantChoisi = parseFloat($('input[name="montant"]:checked').val());
+            let montantReduit = calculerReductionFiscale(montantChoisi);
+            infoReduction.text(`Votre don ne vous coûtera que ${montantReduit} € après réduction fiscale`);
+        });
+
+
+        // Fonction pour mettre à jour les informations de réduction fiscale en fonction du type de don sélectionné
+        function mettreAJourReductionFiscale() {
+            let montantChoisi = parseFloat($('input[name="montant"]:checked').val());
+            let montantReduit = calculerReductionFiscale(montantChoisi);
+            infoReduction.text(`Votre don ne vous coûtera que ${montantReduit} € après réduction fiscale`);
         }
 
         // Événements
+        // Fonction pour afficher les informations de réduction fiscale en fonction du type de don sélectionné
+        function afficherInfosReductionCategorie(categorie) {
+            infoReductionCategorie.text(categorie);
+            let montantChoisi = parseFloat($('input[name="montant"]:checked').val());
+            let montantReduit = calculerReductionFiscale(montantChoisi);
+            infoReduction.text(`Votre don ne vous coûtera que ${montantReduit} € après réduction fiscale`);
+        }
+
+        // Événements pour les boutons de type de don
         btnParticulier.click(function() {
-            $('#champsOrganisme').remove();
-            infoReduction.text("Particulier : vous pouvez bénéficier d’une réduction d’impôt égale à 66 % du montant de votre don , dans la limite de 20 % de votre revenu imposable.");
+            $(this).addClass('btn-selected');
+            btnOrganisme.removeClass('btn-selected');
+            afficherInfosReductionCategorie("Particulier : vous pouvez bénéficier d’une réduction d’impôt égale à 66 % du montant de votre don , dans la limite de 20 % de votre revenu imposable.");
+            payerOrganismeCheckbox.prop('checked', false);
+            cacherChampsOrganisme();
+            mettreAJourReductionFiscale(); // Mettre à jour la réduction fiscale
         });
 
         btnOrganisme.click(function() {
-            ajouterChampsOrganisme();
-            infoReduction.text("Entreprise : l’ensemble des versements à notre association permet de bénéficier d’une réduction d’impôt sur les sociétés de 60 % du montant de ces versements, plafonnée à 20 000 € ou 5 ‰ (5 pour mille) du chiffre d'affaires annuel hors taxe de l’entreprise. En cas de dépassement de plafond, l'excédent est reportable sur les 5 exercices suivants.");
+            $(this).addClass('btn-selected');
+            btnParticulier.removeClass('btn-selected');
+            afficherInfosReductionCategorie("Entreprise : l’ensemble des versements à notre association permet de bénéficier d’une réduction d’impôt sur les sociétés de 60 % du montant de ces versements, plafonnée à 20 000 € ou 5 ‰ (5 pour mille) du chiffre d'affaires annuel hors taxe de l’entreprise. En cas de dépassement de plafond, l'excédent est reportable sur les 5 exercices suivants.");
+            payerOrganismeCheckbox.prop('checked', true);
+            afficherChampsOrganisme();
+            mettreAJourReductionFiscale(); // Mettre à jour la réduction fiscale
+        });
+
+        // Événement pour la case à cocher "Payer en tant qu'organisme"
+        payerOrganismeCheckbox.change(function() {
+            if ($(this).is(':checked')) {
+                btnOrganisme.trigger('click');
+                champsOrganisme.show(); // Afficher les champs pour les organismes si la case est cochée
+            } else {
+                btnParticulier.trigger('click');
+                champsOrganisme.hide(); // Sinon, les masquer
+            }
         });
 
         btnDonMensuel.click(function() {
-            changerMontants({'5': '9', '20': '12', '50': '30', '150': '70'});
+            $(this).addClass('btn-selected');
+            btnDonUneFois.removeClass('btn-selected');
             typeDon.text("Don mensuel");
         });
 
         btnDonUneFois.click(function() {
-            changerMontants({'9': '5', '12': '20', '30': '50', '70': '150'});
+            $(this).addClass('btn-selected');
+            btnDonMensuel.removeClass('btn-selected');
             typeDon.text("Don ponctuel");
+        });
+
+        // Fonction pour mettre à jour le montant total dans le récapitulatif
+        function mettreAJourMontantTotal(montant) {
+            montantTotal.text(`Montant total : ${montant} €`);
+        }
+
+        // Fonction pour gérer le changement de montant choisi
+        function gererChangementMontantChoisi() {
+            let montantChoisi = parseFloat($('input[name="montant"]:checked').val());
+            mettreAJourMontantTotal(montantChoisi);
+            let montantReduit = calculerReductionFiscale(montantChoisi);
+            infoReduction.text(`Votre don ne vous coûtera que ${montantReduit} € après réduction fiscale`);
+        }
+
+        // Gestionnaire d'événement pour le changement de montant choisi
+        montants.change(function() {
+            gererChangementMontantChoisi();
         });
 
         montants.change(function() {
             let montantChoisi = parseFloat($(this).val());
-            miseAJourInfoFiscale(montantChoisi);
-            montantTotal.text(`Montant total: ${montantChoisi} €`);
+            let montantReduit = calculerReductionFiscale(montantChoisi);
+            infoReduction.text(`Votre don ne vous coûtera que ${montantReduit} € après réduction fiscale`);
         });
 
-        montantLibre.change(function() {
+        // Gestionnaire d'événement pour le bouton de confirmation du montant libre
+        $('#confirmMontantLibre').click(function() {
             let montantLibreChoisi = parseFloat(montantLibre.val());
-            miseAJourInfoFiscale(montantLibreChoisi);
-            montantTotal.text(`Montant total: ${montantLibreChoisi} €`);
+            if (!isNaN(montantLibreChoisi)) {
+                $('input[name="montant"]').prop('checked', false); // Désélectionner les autres montants
+                mettreAJourMontantTotal(montantLibreChoisi);
+                let montantReduit = calculerReductionFiscale(montantLibreChoisi);
+                infoReduction.text(`Votre don ne vous coûtera que ${montantReduit} € après réduction fiscale`);
+            }
         });
 
         $('#acceptConditions').change(function() {
             validerPayer.prop('disabled', !$(this).is(':checked'));
         });
     });
-
 </script>
+</body>
 </html>
