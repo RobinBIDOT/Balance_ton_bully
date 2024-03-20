a46aeda711776a3b65efb42abf1a7321c01e00c6
LawsonGod <godwill.lawson@gmail.com>
2024-03-20 12:53:18 +0100

    Mise en forme du formulaire de la page account.php

--- php/account.php
+++ php/account.php
@@ -78,27 +78,52 @@ if(isset($_POST['modMail'])){
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mon compte</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <style>
+        .blue-bg {
+            background-color: #0854C7;
+        }
+    </style>
 </head>
 <body>
     <?php include('../includes/headerNav.php') ?>
-    <form method="post" action="account.php">
-        <p>Nom :</p>
-        <input type="text" value="<?php echo $user['name'] ?>" name="newName"> <input type="submit" value="modifier" name="modName" >
-        <p>Prenom :</p>
-        <input type="text" value="<?php echo $user['firstName'] ?>" name="newFName"><input type="submit" value="modifier" name="modFName">
-        <p>Nom d'utilisateur :</p>
-        <input type="text" value="<?php echo $user['userName'] ?>" name="newUName"><input type="submit" value="modifier" name="modUName" >
-        <p>Adresse mail :</p>
-        <input type="text" value="<?php echo $user['mail'] ?>" name="newMail"><input type="submit" value="modifier" name="modMail">
-        <?php if (isset($_SESSION['nickName'])) { ?> <br> <br>
-        <button type="submit" class="btn btn-danger" name="disconnect">Déconnexion</button>
-        <button type="submit" class="btn btn-danger" name="delete">Supprimer Compte</button>
-        <?php } ?>
-    </form>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="col-md-6 col-lg-4 blue-bg p-4 shadow-lg rounded">
+            <h2 class="text-center text-white">Mon compte</h2>
+            <form method="post" action="account.php">
+                <div class="mb-3">
+                    <label for="newName" class="form-label text-white">Nom :</label>
+                    <input type="text" class="form-control" id="newName" name="newName" value="<?php echo $user['name']; ?>">
+                    <button type="submit" class="btn btn-primary text-white mt-2" name="modName">Modifier</button>
+                </div>
+                <div class="mb-3">
+                    <label for="newFName" class="form-label text-white">Prénom :</label>
+                    <input type="text" class="form-control" id="newFName" name="newFName" value="<?php echo $user['firstName']; ?>">
+                    <button type="submit" class="btn btn-primary text-white mt-2" name="modFName">Modifier</button>
+                </div>
+                <div class="mb-3">
+                    <label for="newUName" class="form-label text-white">Nom d'utilisateur :</label>
+                    <input type="text" class="form-control" id="newUName" name="newUName" value="<?php echo $user['userName']; ?>">
+                    <button type="submit" class="btn btn-primary text-white mt-2" name="modUName">Modifier</button>
+                </div>
+                <div class="mb-3">
+                    <label for="newMail" class="form-label text-white">Adresse mail :</label>
+                    <input type="email" class="form-control" id="newMail" name="newMail" value="<?php echo $user['mail']; ?>">
+                    <button type="submit" class="btn btn-primary text-white mt-2" name="modMail">Modifier</button>
+                </div>
+                    <div class="d-flex justify-content-between mt-4">
+                    <button type="submit" class="btn btn-danger" name="disconnect">Déconnexion</button>
+                    <button type="submit" class="btn btn-danger" name="delete">Supprimer Compte</button>
+                </div>
+
+            </form>
+
+        </div>
+    </div>
     <?php include('../includes/footer.php') ?>
 </body>
 </html>
