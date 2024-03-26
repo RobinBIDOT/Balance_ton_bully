549a1c5316d58d4714d17c268e86c6e5b8d5ec0c
LawsonGod <godwill.lawson@gmail.com>
2024-03-26 15:39:47 +0100

    Maj v2

--- pages/intervention.php
+++ pages/intervention.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Intervention</title>
+    <link rel="stylesheet" href="../css/style-intervention.css">
     <style>
         .blue-bg {
             background-color: #0854C7;
