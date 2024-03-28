a742900e2bee4a753651d9b0eca94d6601148ac0
Robin BIDOT <rbidot@hotmail.fr>
2024-03-28 18:09:25 +0100

    Effet vagues sur le footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,13 @@
 <link rel="stylesheet" href="../css/style.css">
-
+<link rel="stylesheet" href="../css/styleFooter.css">
 <footer class="footer pt-3 py-xl-8">
-
+    <div class="wave-container">
+        <canvas id="canvas"></canvas>
+    </div>
     <div class="container-fluid p-0 mt-2 rounded">
         <div class="container-fluid bg-light">
-
             <div class="row justify-content-center">
                 <div class="col-12 col-md-11">
-
-                    <!-- Widgets - Bootstrap Brain Component -->
                     <section class="py-4 py-md-5 py-xl-8">
                         <div class="container-fluid overflow-hidden">
                             <div class="row gy-4 gy-lg-0 justify-content-xl-between">
@@ -32,13 +31,13 @@
                                         <h4 class="widget-title mb-4">Liens utiles</h4>
                                         <ul class="list-unstyled">
                                             <li class="mb-2">
-                                                <a href="#!" class="text-decoration-none">e-enfance</a>
+                                                <a href="https://e-enfance.org/" class="text-decoration-none" target="_blank">e-enfance</a>
                                             </li>
                                             <li class="mb-2">
-                                                <a href="#!" class="text-decoration-none">Site du gouvernement</a>
+                                                <a href="https://www.education.gouv.fr/non-au-harcelement/politique-de-lutte-contre-le-harcelement-l-ecole-289530" class="text-decoration-none" target="_blank">Site du gouvernement</a>
                                             </li>
                                             <li class="mb-2">
-                                                <a href="#!" class="text-decoration-none">Eduscol</a>
+                                                <a href="https://eduscol.education.fr/" class="text-decoration-none" target="_blank">Eduscol</a>
                                             </li>
                                         </ul>
                                     </div>
@@ -55,8 +54,6 @@
                             </div>
                         </div>
                     </section>
-
-                    <!-- Copyright - Bootstrap Brain Component -->
                     <div class="py-4 py-md-5 py-xl-8 border-top border-light-subtle">
                         <div class="container-fluid overflow-hidden">
                             <div class="row gy-4 gy-md-0">
@@ -64,11 +61,7 @@
                                     <div class="copyright text-center text-md-start">
                                         &copy; 2024. All Rights Reserved.
                                     </div>
-                                    <div class="credits text-secondary text-center text-md-start mt-2 fs-7">
-                                        Built by <a href="https://bootstrapbrain.com/" class="link-secondary text-decoration-none">Balance ton bully</a> with <span class="text-primary">&#9829;</span>
-                                    </div>
                                 </div>
-
                                 <div class="col-xs-12 col-md-5 order-0 order-md-1">
                                     <ul class="nav justify-content-center justify-content-md-end" style="--bs-nav-link-padding-x: .8rem;">
                                         <li class="nav-item">
@@ -110,3 +103,131 @@
         </div>
     </div>
 </footer>
+<script>
+    /**
+     * Fonction auto-invoquée pour encapsuler le code
+     */
+    (function() {
+        "use strict";
+
+        // Déclaration de variables
+        var cvs, ctx;
+        var nodes = 6;
+        var waves = [];
+        var waveHeight = 60;
+        var colours = ["#0854C7", "#0A6DD9", "#0B77EB"]; // Couleurs des vagues
+
+        /**
+         * Initialise le canvas et commence le processus de mise à jour des vagues.
+         */
+        function init() {
+            cvs = document.getElementById("canvas"); // Récupération du canvas
+            ctx = cvs.getContext("2d"); // Obtention du contexte 2D
+            resizeCanvas(cvs); // Réglage de la taille du canvas
+
+            // Création des vagues avec différentes couleurs
+            for (var i = 0; i < 3; i++) {
+                waves.push(new wave(colours[i], 1, nodes));
+            }
+
+            update(); // Démarrage du processus de mise à jour
+        }
+
+        /**
+         * Mise à jour et redessine les vagues sur le canvas.
+         */
+        function update() {
+            var fill = "#A42AF0"; // Couleur de fond pour la vague
+            ctx.fillStyle = fill;
+            ctx.globalCompositeOperation = "source-over";
+            ctx.fillRect(0, 0, cvs.width, cvs.height);
+            ctx.globalCompositeOperation = "screen";
+
+            // Mise à jour et dessin de chaque vague
+            for (var i = 0; i < waves.length; i++) {
+                for (var j = 0; j < waves[i].nodes.length; j++) {
+                    bounce(waves[i].nodes[j]);
+                }
+                drawWave(waves[i]);
+            }
+
+            requestAnimationFrame(update); // Appel récursif pour la prochaine mise à jour
+        }
+
+        /**
+         * Représente une vague.
+         * @param {string} colour - Couleur de la vague
+         * @param {number} lambda - Inutilisé dans cette version
+         * @param {number} nodes - Nombre de nœuds dans la vague
+         */
+        function wave(colour, lambda, nodes) {
+            this.colour = colour;
+            this.lambda = lambda;
+            this.nodes = [];
+
+            // Création des nœuds de la vague
+            for (var i = 0; i <= nodes + 2; i++) {
+                var temp = [(i - 1) * cvs.width / nodes, 0, Math.random() * 200, .3];
+                this.nodes.push(temp);
+            }
+        }
+
+        /**
+         * Fait rebondir un nœud de la vague.
+         * @param {Array} nodeArr - Tableau représentant un nœud de la vague
+         */
+        function bounce(nodeArr) {
+            nodeArr[1] = waveHeight / 2 * Math.sin(nodeArr[2] / 20) + cvs.height / 2;
+            nodeArr[2] = nodeArr[2] + nodeArr[3];
+        }
+
+        /**
+         * Dessine une vague sur le canvas.
+         * @param {Object} obj - Objet représentant une vague
+         */
+        function drawWave(obj) {
+            var diff = function(a, b) {
+                return (b - a) / 2 + a;
+            }
+
+            ctx.fillStyle = obj.colour;
+            ctx.beginPath();
+            ctx.moveTo(0, cvs.height);
+            ctx.lineTo(obj.nodes[0][0], obj.nodes[0][1]);
+
+            for (var i = 0; i < obj.nodes.length; i++) {
+                if (obj.nodes[i + 1]) {
+                    ctx.quadraticCurveTo(
+                        obj.nodes[i][0], obj.nodes[i][1],
+                        diff(obj.nodes[i][0], obj.nodes[i + 1][0]), diff(obj.nodes[i][1], obj.nodes[i + 1][1])
+                    );
+                } else {
+                    ctx.lineTo(obj.nodes[i][0], obj.nodes[i][1]);
+                    ctx.lineTo(cvs.width, cvs.height);
+                }
+            }
+            ctx.closePath();
+            ctx.fill();
+        }
+
+        /**
+         * Ajuste la taille du canvas en fonction de la largeur de la fenêtre.
+         * @param {HTMLElement} canvas - L'élément canvas à redimensionner
+         */
+        function resizeCanvas(canvas) {
+            if (window.innerWidth > 1920) {
+                canvas.width = window.innerWidth;
+            } else {
+                canvas.width = 1920;
+            }
+            canvas.height = waveHeight;
+        }
+
+        // Initialisation lorsque le DOM est prêt
+        document.addEventListener("DOMContentLoaded", function() {
+            if (document.getElementById("canvas")) {
+                init();
+            }
+        });
+    })();
+</script>
