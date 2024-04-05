64dd64bc9d71d50889b6b59b4a846f665b30e72c
LawsonGod <godwill.lawson@gmail.com>
2024-04-05 15:06:52 +0200

    modif du style button menuburger

--- includes/headerNav.php
+++ includes/headerNav.php
@@ -49,7 +49,7 @@
     <div class="container-fluid p-0 rounded">
         <nav class="navbar navbar-expand-lg navbar-light bg-light rounded-bottom">
             <!-- Bouton pour le menu burger -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler mx-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <!-- Contenu du menu -->
