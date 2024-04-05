217bbf3c170d2ed5240a80519163131aba14eddd
Robin BIDOT <rbidot@hotmail.fr>
2024-04-05 12:31:59 +0200

    correction du footer

--- pages/actualites.php
+++ pages/actualites.php
@@ -218,7 +218,6 @@ $actualities = rechercherActualites($dbh, $motCle, $offset, $elementsParPage);
     }
     ?>
 </div>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
 <?php include('../includes/footer.php') ?>
 <?php include('../includes/scriptLink.php') ?>
 <script>
